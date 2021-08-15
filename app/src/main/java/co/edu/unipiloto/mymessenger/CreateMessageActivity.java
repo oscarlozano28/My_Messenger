d921eb6751c91eaec861c5bdce62a2744d20b2b1
oscarlozano28 <oscar-lozano1@upc.edu.co>
2021-08-15 09:28:24 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,27 @@
+package co.edu.unipiloto.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.lang.annotation.Target;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage (View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
